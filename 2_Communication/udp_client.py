bd014f57163da9b05a565380a4efec3abac8fad1
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-04-30 17:05:09 +0900

    changed

--- 2_Communication/udp_client.py
+++ 2_Communication/udp_client.py
@@ -1,7 +1,7 @@
 import socket
 
 target_host = "0.0.0.0"
-target_port = 9997
+target_port = 9998
 
 # make socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
---
+++ 3_Network/sniffer.py
@@ -0,0 +1,31 @@
+import socket
+import os
+
+# IP address to listen to
+HOST = "localhost"
+
+def main():
+    # create raw socket, and bint to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+    # Specifies that the IP header should be included in the capture result
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # read packet
+    print(sniffer.recvfrom(65565))
+
+    # Disable promiscuous mode for Windows.
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
+if __name__ == '__main__':
+    main()
